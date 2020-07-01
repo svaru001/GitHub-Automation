b6b728b9f74cad6d4dfb30de312e49a445705200
Sanket <sv@smartkarma.com>
2020-07-01 14:11:46 +0800

    Fixed docker configurations to run tests in parallel

--- run-configs/docker.config.js
+++ run-configs/docker.config.js
@@ -43,7 +43,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 1,
+    maxInstances: 8,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -54,26 +54,26 @@ exports.config = {
           // maxInstances can get overwritten per capability. So if you have an in-house Selenium
           // grid with only 5 Chrome instances available you can make sure that not more than
           // 5 instances get started at a time.
-          maxInstances: 1,
+          maxInstances: 3,
           browserName: 'chrome',
           'goog:chromeOptions': {
             // to run chrome headless the following flags are required
             // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-             args: ['--headless', '--disable-gpu'],
+             //args: ['--headless', '--disable-gpu'],
           }
         },
   
-        {
-          // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-          // grid with only 5 firefox instances available you can make sure that not more than
-          // 5 instances get started at a time.
-          maxInstances: 1,
-          browserName: 'firefox',
-          "moz:firefoxOptions": {
-            // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
-            args: ['-headless']
-          }
-        },
+        // {
+        //   // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+        //   // grid with only 5 firefox instances available you can make sure that not more than
+        //   // 5 instances get started at a time.
+        //   maxInstances: 1,
+        //   browserName: 'firefox',
+        //   "moz:firefoxOptions": {
+        //     // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
+        //     args: ['-headless']
+        //   }
+        // },
   
 
   
